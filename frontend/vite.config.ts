063c45bbf0d05dd22e72b48f4e3aaa64aefaf2c4
Walter <106196739+waltercamino@users.noreply.github.com>
2025-08-21 17:54:19 -0300

    correcion en el archivo vite.config.js tailwind.config.js y postcss.config.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // indica que el root es `frontend/`
-  server: {
-    port: 5173,
-    open: true,
-  },
 });
