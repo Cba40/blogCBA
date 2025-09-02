2dbc13c165a14ac1dcb287a9d7603da50a589fc1
Walter <106196739+waltercamino@users.noreply.github.com>
2025-09-01 21:09:56 -0300

    cambio de rutas en vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/blog/', // ✅ Esencial para que funcione en Don Web
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
 });
